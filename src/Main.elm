4d7b55c4108b5605de18210112f169d397bb96f1
Thibaut Gery <thibaut.gery@gmail.com>
2017-02-25 15:55:51 +1100

    split Main

--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,10 @@
 module Main exposing (..)
 
 import Html exposing (Html, div, text, program)
-
-
--- MODEL
-
-
-type alias Model =
-    String
+import Msgs exposing (Msg)
+import Models exposing (Model)
+import Update exposing (update)
+import View exposing (view)
 
 
 init : ( Model, Cmd Msg )
@@ -15,46 +12,9 @@ init =
     ( "Hello", Cmd.none )
 
 
-
--- MESSAGES
-
-
-type Msg
-    = NoOp
-
-
-
--- VIEW
-
-
-view : Model -> Html Msg
-view model =
-    div []
-        [ text model ]
-
-
-
--- UPDATE
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        NoOp ->
-            ( model, Cmd.none )
-
-
-
--- SUBSCRIPTIONS
-
-
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
-
-
-
--- MAIN
 
 
 main : Program Never Model Msg
