566aefea84f0b6f49470dea2d1d8cef35b9703f3
Thibaut Gery <thibaut.gery@gmail.com>
2017-02-25 16:07:16 +1100

    use only compenent in semantic UI

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 'use strict';
 
-require('semantic-ui-css/semantic.min.css')
-require('font-awesome/css/font-awesome.css');
+
+const semanticUiComponenet = ["grid", "icon"]
+
+semanticUiComponenet.forEach(component -> {
+  require('semantic-ui-css/components/' + component + '.min.css')
+})
 
 // Require index.html so it gets copied to dist
 require('./index.html');
 
-var Elm = require('./Main.elm');
-var mountNode = document.getElementById('main');
+const Elm = require('./Main.elm');
+const mountNode = document.getElementById('main');
 
 // .embed() can take an optional second argument. This would be an object describing the data we need to start a program, i.e. a userID or some token
-var app = Elm.Main.embed(mountNode);
+const app = Elm.Main.embed(mountNode);
